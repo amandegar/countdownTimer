5f7a033077768cdc71a672eef05d5a52d33cf3d7
Amir <amandegar@gmail.com>
2019-04-08 22:08:41 -0400

    Create new dialog for setting timer value

---
+++ newtimedialog.cpp
@@ -0,0 +1,6 @@
+#include "newtimedialog.h"
+
+newTimeDialog::newTimeDialog()
+{
+
+}
---
+++ newtimedialog.h
@@ -0,0 +1,11 @@
+#ifndef NEWTIMEDIALOG_H
+#define NEWTIMEDIALOG_H
+
+
+class newTimeDialog
+{
+public:
+    newTimeDialog();
+};
+
+#endif // NEWTIMEDIALOG_H
