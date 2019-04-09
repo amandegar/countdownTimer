73ee698de6ecc8e418f299105c61f3a4a8096ffe
Amir <amandegar@gmail.com>
2019-04-09 19:51:51 -0400

    cleanup and refactoring

--- analogclock.cpp
+++ analogclock.cpp
@@ -1,59 +1,8 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** BSD License Usage
-** Alternatively, you may use this file under the terms of the BSD license
-** as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
-
 #include <QtWidgets>
 
 #include "analogclock.h"
 //----------------------------------------
-AnalogClock::AnalogClock(QWidget *parent)
+AnalogCountdown::AnalogCountdown(QWidget *parent)
     : QWidget(parent)
 {
     timeDialog = new newTimeDialog(this);
@@ -81,8 +30,15 @@ AnalogClock::AnalogClock(QWidget *parent)
     new QShortcut(QKeySequence(Qt::Key_F), this, SLOT(shortcutFullScreen()));
     new QShortcut(QKeySequence(Qt::Key_S), this, SLOT(shortcutSetTimer()));
 }
+
+void AnalogCountdown::resetTimerValue(int newMinute)
+{
+    countdownSetValue.setHMS(0,newMinute,0);
+    countdownCurrentValue = countdownSetValue;
+    timer->stop();
+}
 //----------------------------------------
-void AnalogClock::soundAlert()
+void AnalogCountdown::soundAlert()
 {
 // 3 minutes alert
     if (countdownCurrentValue.minute() == 3 and countdownCurrentValue.second() == 0)
@@ -104,7 +60,7 @@ void AnalogClock::soundAlert()
         QApplication::beep();
 }
 //----------------------------------------
-void AnalogClock::timerTrigger()
+void AnalogCountdown::timerTrigger()
 {
     if (countdownCurrentValue.minute() == 0 and countdownCurrentValue.second() <= 0)
         timer->stop();
@@ -115,19 +71,16 @@ void AnalogClock::timerTrigger()
     this->update();
 }
 //----------------------------------------
-void AnalogClock::shortcutReset()
+void AnalogCountdown::shortcutReset()
 {
     int ret = QMessageBox::warning(this, tr("Timer reset"),
                                     tr("Are you sure to reset your timer?"),
                                     QMessageBox::Yes | QMessageBox::No, QMessageBox::No);
     if (ret == QMessageBox::Yes)
-    {
-        countdownCurrentValue = countdownSetValue;
-        timer->stop();
-    }
+        resetTimerValue(countdownSetValue.minute());
 }
 //----------------------------------------
-void AnalogClock::shortcutStartStop()
+void AnalogCountdown::shortcutStartStop()
 {
     if(timer->isActive())
     {
@@ -141,7 +94,7 @@ void AnalogClock::shortcutStartStop()
     }
 }
 //----------------------------------------
-void AnalogClock::shortcutTopWindow()
+void AnalogCountdown::shortcutTopWindow()
 {
     if (!topWindow)
     {
@@ -156,7 +109,7 @@ void AnalogClock::shortcutTopWindow()
     show();
 }
 //----------------------------------------
-void AnalogClock::shortcutAboutDialog()
+void AnalogCountdown::shortcutAboutDialog()
 {
     QString aboutTxt("Fenikkusu Countdown Timer\n"
                      "Version 0.1 BETA\n"
@@ -175,36 +128,34 @@ void AnalogClock::shortcutAboutDialog()
     QMessageBox::about(this, "About", aboutTxt);
 }
 //----------------------------------------
-void AnalogClock::shortcutZoomOut()
+void AnalogCountdown::shortcutZoomOut()
 {
     QSize additionalSize(100,100);
     this->resize(this->size() - additionalSize);
 }
 //----------------------------------------
-void AnalogClock::shortcutFullScreen()
+void AnalogCountdown::shortcutFullScreen()
 {
     if (this->windowState().testFlag(Qt::WindowMaximized))
         this->showNormal();
     else
         this->showMaximized();
+//        this->showFullScreen();
 }
 //----------------------------------------
-void AnalogClock::shortcutSetTimer()
+void AnalogCountdown::shortcutSetTimer()
 {
-    timeDialog->show();
+    if (timeDialog->exec())
+        resetTimerValue(timeDialog->getMinute());
 }
 //----------------------------------------
-void AnalogClock::shortcutZoomIn()
+void AnalogCountdown::shortcutZoomIn()
 {
-//    this->showFullScreen();
-//    this->resize(400,400);
-//    this->showMaximized();
-
    QSize additionalSize(100,100);
    this->resize(this->size() + additionalSize);
 }
 //----------------------------------------
-void AnalogClock::paintEvent(QPaintEvent *)
+void AnalogCountdown::paintEvent(QPaintEvent *)
 {
     const QPoint minuteHand[3] = {
         QPoint(7, 8),
--- main.cpp
+++ main.cpp
@@ -56,7 +56,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    AnalogClock clock;
+    AnalogCountdown clock;
     clock.show();
     return app.exec();
 }
